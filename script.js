13c86bc4b62cb4ee94e2b3749be9da5552421c62
devfaizan <mfaizan.zahir@gmail.com>
2024-08-13 02:29:55 +0500

    add project details & images folder

--- script.js
+++ script.js
@@ -85,3 +85,39 @@ document.addEventListener('DOMContentLoaded', (event) => {
     crossButton.addEventListener('click', closeMobileMenu);
     loadTheme();
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var modal = document.getElementById('imageModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var modalImg = document.getElementById('img01');
+    var images = document.querySelectorAll('.p-img');
+
+    images.forEach(img => {
+        img.onclick = function () {
+            modal.style.display = 'block';
+            modalImg.src = this.src;
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById('closeModal');
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
+
+    // When the user clicks anywhere outside of the modal content, close the modal
+    modal.onclick = function (event) {
+        if (event.target !== modalImg) {
+            modal.style.display = 'none';
+            document.body.style.overflow = 'auto';
+        }
+    }
+});
+
